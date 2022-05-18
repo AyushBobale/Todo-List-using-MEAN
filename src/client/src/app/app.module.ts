f8607132c22c9015abade83705ef2b1b935915c1
Ayush Bobale <bobaleayush@gmail.com>
2022-05-18 22:25:46 +0530

    Routing

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { MainbodyComponent } from './components/mainbody/mainbody.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: MainbodyComponent},
+  {path: 'projects', component: ProjectsComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainbodyComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
